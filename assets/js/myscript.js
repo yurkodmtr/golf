14f31f29696ef0f4afc6a2d5cfab399ef7397dd4
Yurko Dmitriev <4au.4au.4au@gmail.com>
2018-02-20 22:12:33 +0200

    33

--- assets/js/myscript.js
+++ assets/js/myscript.js
@@ -1,3 +1,22 @@
-$(window).load(function(){
-	
-});
+"use strict";
+
+var customScript = function(){
+
+	/* remove placeholder */
+	var removePlaceholder = function () {
+	    $('input,textarea').focus(function () {
+	        $(this).data('placeholder', $(this).attr('placeholder')).attr('placeholder', '');
+	    }).blur(function () {
+	        $(this).attr('placeholder', $(this).data('placeholder'));
+	    });
+	}
+
+	$(document).ready(function(){
+		removePlaceholder();
+	  	$(".owl-header_gal").owlCarousel({
+	  		'items':1
+	  	});
+	});
+}
+
+customScript();
