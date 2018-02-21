db63ba969348b78a51bdf68f09b4a4b3cec399b3
yurkoDMTR <4au.4au.4au@gmail.com>
2018-02-21 17:29:08 +0200

    55

--- assets/js/myscript.js
+++ assets/js/myscript.js
@@ -11,11 +11,76 @@ var customScript = function(){
 	    });
 	}
 
-	$(document).ready(function(){
+	/* autoHeight */
+	var autoHeight = function(elem){
+		if ( elem.length < 1 ) {
+			return false;
+		}
+		elem.css('height','auto');
+		var maxHeight = 0;
+		elem.each(function(){
+			var elemHeight = $(this).height();
+			if ( elemHeight >= maxHeight) {
+				maxHeight = elemHeight;
+			}
+		});
+		elem.css('height', maxHeight+'px');
+	}
+
+	/* userToggle */
+	var userToggle = function(){
+		$('._user__toggle').click(function(){
+			if ( $('._user').hasClass('act') ) {
+				$('._user').removeClass('act');
+				$('._user__content').hide();
+			} else {
+				$('._user').addClass('act');
+				$('._user__content').fadeIn('fast');
+			}
+		});
+	}
+
+	/* headerDrop */
+	var	headerDrop = function(){
+		$('.header__drop__title').click(function(){
+			if ( $(this).parent().hasClass('act') ) {
+				$(this).parent().removeClass('act');
+				$(this).next().hide();
+			} else {
+				$(this).parent().addClass('act');
+				$(this).next().fadeIn('fast');
+			}
+		});
+	}
+
+	$(document).ready(function(){	
+		userToggle();	
+		headerDrop();
 		removePlaceholder();
+
+		/* carousel init */
 	  	$(".owl-header_gal").owlCarousel({
-	  		'items':1
+	  		'items':1,
+	  		'pagintaion':true
 	  	});
+	  	$(".owl-instructors_block").owlCarousel({
+	  		'items':2,
+	  		'nav': true
+	  	});
+	  	$(".owl-technologies_block").owlCarousel({
+	  		'items':1,
+	  		'nav':true
+	  	});
+	});
+
+	$(window).load(function(){
+		autoHeight($('.lessons_block .item .title'));
+		autoHeight($('.lessons_block .item .descr'));
+	});
+
+	$(window).resize(function(){
+		autoHeight($('.lessons_block .item .title'));
+		autoHeight($('.lessons_block .item .descr'));
 	});
 }
 
